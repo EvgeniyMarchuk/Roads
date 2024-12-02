ac1b844aa1f2b57bdd5b632dc32dea6826a52caa
nikita <ne24kit@gmail.com>
2024-12-03 00:31:50 +0300

    Generate photorealistic roads based on mask from module1 by using stable diffusion and controlnet

---
+++ modul2.py
@@ -0,0 +1,45 @@
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+from diffusers.utils import load_image
+import torch
+from PIL import Image, ImageOps
+
+
+
+#TODO: Обернуть всё в функцию
+def generate_realistic_road():
+    pass
+
+
+# Загрузка ControlNet модели для сегментации
+controlnet = ControlNetModel.from_pretrained(
+    "lllyasviel/sd-controlnet-seg", torch_dtype=torch.float16
+)
+
+# Загрузка основной модели
+pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
+)
+pipe.to("cuda")
+
+# Загрузка и обработка маски
+mask_image = load_image("./masks/w_20 down_376_511 right_511_178.png").resize((512, 512))
+# mask_image = ImageOps.grayscale(mask_image).point(lambda x: 255 if x > 128 else 0, mode='1')
+
+# Установка параметров генерации
+prompt = (
+    "ONLY ONE ROAD! A highly detailed photorealistic aerial view of a smooth highway, "
+    "winding through a dense forest. Satellite perspective, high resolution. "
+)
+
+prompt1 = "Only one motorway on the given mask around the forest. Vertical view from above."
+
+generated_image = pipe(
+    prompt1,
+    image=mask_image,
+    num_inference_steps=50,
+    guidance_scale=7.5,
+    control_strength=0.9 # Регулирует влияние маски
+).images[0]
+
+# Сохранение результата
+generated_image.save("final_generated_road.png")
